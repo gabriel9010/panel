a6ab4af11b9ec4067278b0e632b1ad53b4f140ed
Gabriel Claudio Fogliano <86239411+gabriel9010@users.noreply.github.com>
2024-08-21 06:52:39 +0200

    update tentativo public

--- my-app-backend/server.js
+++ my-app-backend/server.js
@@ -1,6 +1,4 @@
-const https = require('https');
 const http = require('http');
-const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -11,16 +9,29 @@ const candidatureRoutes = require('./routes/candidatureRoutes');
 const { protect } = require('./middleware/authMiddleware'); // Middleware di protezione
 const whitelistMiddleware = require('./middleware/whitelistMiddleware'); // Middleware per la whitelist degli IP
 
+// Carica le variabili d'ambiente
 dotenv.config();
+
+// Connessione al database
 connectDB();
 
 const app = express();
+
+// Middleware per gestire JSON e CORS
 app.use(cors());
 app.use(express.json());
 
-// Configura il motore di visualizzazione come EJS
+// Configura il motore di visualizzazione come EJS (se necessario per altre pagine)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Configurazione della cartella statica per servire file HTML, CSS, JS, ecc.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rotta per servire la pagina home.html alla root "/"
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html')); // Servire home.html
+});
 
 // Rotte di autenticazione
 app.use('/api/auth', authRoutes);
@@ -42,11 +53,13 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Imposta la porta per il server HTTP
 const PORT = process.env.PORT || 5000;
-
 
 // Configurazione per il server HTTP
 const httpServer = http.createServer(app);
+
+// Avvia il server HTTP
 httpServer.listen(PORT, () => {
   console.log(`HTTP Server running on port ${PORT}`);
 });
